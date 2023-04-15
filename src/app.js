e9d7c496be9b2352f91eb2eccb20e2e1999c14af
Kelvin Ruiz <kelvinruiz.developer@gmail.com>
2023-04-14 23:41:14 -0800

    Variable global a local, reducción de comentarios

--- src/app.js
+++ src/app.js
@@ -2,11 +2,6 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-let excuse = "";
-
 let who = ["The dog", "My grandma", "His turtle", "My bird"];
 let action = ["ate", "peed", "crushed", "broke"];
 let what = ["my homework", "the keys", "the car"];
@@ -19,25 +14,17 @@ let when = [
 ];
 
 function excuseGenerator() {
-  excuse = who[Math.floor(Math.random() * who.length)] + " ";
-  console.log(Math.floor(Math.random() * who.length));
+  let excuse = "";
+  //Genera excusa
+  excuse =  who[Math.floor(Math.random() * who.length)] + " ";
   excuse += action[Math.floor(Math.random() * action.length)] + " ";
-
   excuse += what[Math.floor(Math.random() * what.length)] + " ";
-
   excuse += when[Math.floor(Math.random() * when.length)] + ".";
 
-  //Se usa la funcion array.length para generar
-  //un numero aleatorio que comprenda el numero
-  //de elementos del array haciendolo dinamico,
-  //esto en caso modificar la cantidad de elementos
-  //de algun array.
   return excuse;
 }
 
 window.onload = function() {
-  //write your code here
-  //console.log(excuseGenerator());
   //Muestra la excusa generada en el html
   var objetivo = document.getElementById("excuse");
   objetivo.innerHTML = excuseGenerator();
